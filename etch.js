af2641b2fc2660a50e2baff6e89323b6542a8de5
nearlyblank <nearlyblank@protonmail.com>
2022-06-14 22:16:24 -0700

    got the prompt and box generation working

--- etch.js
+++ etch.js
@@ -14,14 +14,35 @@
 
 // }
 
+document.addEventListener("DOMContentLoaded", function() {
+    makeRow();
+  });
 
+let dim = parseInt(prompt('Please enter size of grid', '16' ));
+
+if (dim > 100) {
+    alert("Number must be less than 100 and greater than 0");
+    dim = parseInt(prompt('Please enter size of grid', '16'));
+}
+
+else if (dim < 0) {
+    alert("Number must be less than 100 and greater than 0");
+    dim = parseInt(prompt('Please enter size of grid', '16'));
+}
 
 const gridContainer = document.querySelector('.grid-container');
-let gridColumn = document.getElementsByClassName('grid-column');
+const rowContainer = document.querySelector('.row-container');
+
+// let gridColumn = document.getElementsByClassName('grid-column');
 let gridBox = document.getElementsByClassName('grid-box');
 
+function makeRow() {
+    for (let i = 0; i < dim; i++) {
+        let newBox = document.createElement('div');
+        rowContainer.appendChild(newBox).className = "grid-box";
+    }
+}
 
-for (let i = 0; i<16; i++) {
-    let newBox = document.createElement('div');
-    gridContainer.appendChild(newBox).className = "grid-box";
+function makeGrid() {
+
 }
