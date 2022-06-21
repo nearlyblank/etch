c909e7cf224d0a77239477e6138d728e558fe086
nearlyblank <nearlyblank@protonmail.com>
2022-06-20 23:34:31 -0700

    starting over, grid now populates correctly

--- etch.js
+++ etch.js
@@ -1,53 +1,52 @@
 let regex = /^[1-9][0-9]{0,1}$/; //regex limiting range of valid entries of 1 to 99
 
-const dimBtn = document.getElementsByClassName('dim-btn');
+const gridBtn = document.getElementsByClassName('grid-btn');
+const board = document.querySelector('.board');
+const rows = document.getElementsByClassName('row');
 
 
-dimBtn[0].addEventListener('click', () => {
+gridBtn[0].addEventListener('click', () => {
     // const gridBox = document.getElementsByClassName('grid-box');
     // div.style.removeProperty('background-color');
     //gridBox[0].style.backgroundColor = null; //this should work once the default grid function works
     userInput = prompt('Please enter size of grid', '16' );
-    userGrid(userInput);
+    userGrid();
 });
 
 function userGrid() {
-    if (userInput.match(regex) == null) {
-        alert("Number must be less than 100 and greater than 0");
-        userInput = prompt('Please enter size of grid', '16');
-        makeRow(userInput);
+    if (userInput.match(regex) === null) {
+        alert("Number must be less than 100 and greater than 3");
     }
-    else if (userInput.match(regex)!= null) {
-    makeRow(userInput);
+    else {
+        makeRow(userInput);
+        makeColumn(userInput);
     }
 };
 
-
-
-const gridContainer = document.querySelector('.grid-container');
-const rowContainer = document.querySelector('.row-container');
-
-// let gridColumn = document.getElementsByClassName('grid-column');
-
-
-
-
-function makeRow(num) {
-    for (let i = 0; i < num; i++) {
-        let newBox = document.createElement('div');
-        rowContainer.appendChild(newBox).className = "grid-box";
-        newBox.addEventListener('mouseover', function(e) {
-            e.target.style.backgroundColor = 'white';
-        });
+function makeRow (rowNum){
+    for (let i = 0; i < userInput; i++) {
+        const newRow = document.createElement('div');
+        board.appendChild(newRow).className = "row";
     }
 }
 
-// function reset (gridBox) {
-//     gridBox = document.getElementsByClassName('grid-box');
-//     gridBox[0].style.removeProperty('background-color');
-// }
-function makeGrid(num) {
-    makeRow(num*num)
+function makeColumn (newRow) {
+    for (let i = 0; i < userInput; i++) {
+        for (let j = 0; j < userInput; j++) {
+            const newBox = document.createElement('div');
+            rows[j].appendChild(newBox).className = "grid-box";
+        }
+    }
 }
 
-//next: figure out reset function > run reset at beginning of click event, create default grid function that loads at beginning (makeRow(16))
+
+function defaultGrid () {
+    makeRow(16);
+    makeColumn(16);
+}
+
+function colorChange () {
+    //call this function to create
+}
+
+//next: update regex for 4-99, add event listener mouseover, add clear function to call on click
