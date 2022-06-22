95e165cba5e3fc612b606b62caefa0bbf509ad03
nearlyblank <nearlyblank@protonmail.com>
2022-06-22 00:15:15 -0700

    cleaned up a bit, added mouseover event listener

--- etch.js
+++ etch.js
@@ -4,58 +4,57 @@ const gridBtn = document.getElementsByClassName('grid-btn');
 const board = document.querySelector('.board');
 const rows = document.getElementsByClassName('row');
 let userInput = 16;
-
+makeGrid();
 
 gridBtn[0].addEventListener('click', () => {
-    clearBoard();
-    // const gridBox = document.getElementsByClassName('grid-box');
-    // div.style.removeProperty('background-color');
-    //gridBox[0].style.backgroundColor = null; //this should work once the default grid function works
     userInput = prompt('Please enter size of grid', '16' );
     userGrid();
 });
-
-
 
 function userGrid() {
     if (userInput.match(regex) === null) {
         alert("Number must be less than 100 and greater than 3");
     }
     else {
-        makeRow(userInput);
-        makeColumn(userInput);
+        clearBoard();
+        makeGrid();
     }
-};
+}
 
-function makeRow (rowNum){
+function makeRow (){
     for (let i = 0; i < userInput; i++) {
         const newRow = document.createElement('div');
         board.appendChild(newRow).className = "row";
     }
 }
 
-function makeColumn (newRow) {
-    for (let i = 1; i < userInput; i++) {
+function makeColumn () {
+    for (let i = 0; i < userInput; i++) {
         for (let j = 0; j < userInput; j++) {
             const newBox = document.createElement('div');
             rows[j].appendChild(newBox).className = "grid-box";
+            newBox.addEventListener('mouseover', function(e) {
+                e.target.style.backgroundColor = 'white';
+            });
         }
     }
 }
 
-
-function defaultGrid () {
-    makeRow(16);
-    makeColumn(16);
+function makeGrid () {
+    makeRow();
+    makeColumn();
 }
-defaultGrid();
 
 function clearBoard () {
     board.replaceChildren();
 }
 
 function colorChange () {
-    //call this function to create
+
 }
 
-//next:  add event listener mouseover
+function erase () {
+
+}
+
+//next:  add erase button, color change button
